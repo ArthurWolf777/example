703314964cb3542cc1c61ea8a23e7f5bcb0b0912
ArthurWolf777 <117633586+ArthurWolf777@users.noreply.github.com>
2023-01-25 02:03:21 -0600

    Debug FALSE

--- agenda/settings.py
+++ agenda/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-l%e20z4bses8+u%e!_qovg24^fe1^++as4_of!t-f_5ji!e7!*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
